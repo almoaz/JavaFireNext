2e0586604175c5150b459b5edde7ccdd6ee5d072
Mahfuj Salehin Moaz <almuaz1998@gmail.com>
2022-12-14 16:09:06 +0600

    main

---
+++ src/main.java
@@ -0,0 +1,49 @@
+import FireNext.NextDatabase;
+public class main {
+
+    public static void main(String[] args) {
+        print(NextDatabase.add("USER"));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private static void print(String value) {
+        System.out.println(value);
+    }
+    private static void print(int value) {
+        System.out.println(value);
+    }
+}
